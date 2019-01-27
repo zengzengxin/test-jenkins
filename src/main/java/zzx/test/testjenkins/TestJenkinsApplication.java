a30440cf9f4c413013a8bb9599047696b7389f3d
zengzengxin <cengzengxin@talkmoney.cn>
2019-01-27 11:26:06 +0800

    Initial commit

---
+++ src/main/java/zzx/test/testjenkins/TestJenkinsApplication.java
@@ -0,0 +1,14 @@
+package zzx.test.testjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinsApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/zzx/test/testjenkins/controller/helloController.java
@@ -0,0 +1,16 @@
+package zzx.test.testjenkins.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class helloController {
+
+    @RequestMapping("hello")
+    public String sayHello(){
+        return "success hello word ";
+    }
+}
